13243f4695a4d9a75d300978553ba5eb50776073
Ramsha <fnuramsha@gmail.com>
2024-05-14 18:00:09 +0200

    completed post part

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 "use strict";
 
-const displayPost = function (showData) {};
+const container = document.querySelector(".container");
+const title = document.querySelector(".element");
+const body = document.querySelector(".lists");
+
+const displayPost = function (showTitle, showText) {
+  const divCreate = document.createElement("div");
+  container.append(divCreate);
+  const imgElement = document.createElement("img");
+  imgElement.src = "profile.png";
+  divCreate.append(imgElement);
+  const titleCreation = document.createElement("h4");
+  titleCreation.textContent = showTitle;
+  divCreate.append(titleCreation);
+  const bodyCreation = document.createElement("p");
+  bodyCreation.textContent = showText;
+  divCreate.append(bodyCreation);
+};
 
 const getPost = async function () {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     const posts = await response.json();
-    // console.log(posts);
+
     for (let i = 0; i < posts.length; i++) {
       const newPost = posts[i];
-      console.log(newPost.title, newPost.body);
+
+      displayPost(newPost.title, newPost.body);
     }
   } catch (err) {
     console.log(err);
